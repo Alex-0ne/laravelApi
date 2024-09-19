e73f216dff30f22a4bb4b09dfe17f5cdf2aa11fa
Jose Alexander Rodriguez Navarrete <alex25cobaem@gmail.com>
2024-09-19 09:34:39 -0600

    Rutas para recetas, etiquetas y categorias, con la relacion de los modelos y las funciones

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoriaController;
+use App\Http\Controllers\Api\RecetaController;
+use App\Http\Controllers\Api\EtiquetaController;
 
+Route::get('categorias', [CategoriaController::class, 'index']);
+Route::get('categorias/{categoria}', [CategoriaController::class, 'show']);
+
+Route::apiResource('etiquetas', EtiquetaController::class);
+
+
+Route::get('recetas', [RecetaController::class, 'index']);
+Route::get('recetas/{receta}', [RecetaController::class, 'show']);
+/*
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
